bae2a77640d20321101df294899a7838362f3a38
DavidSalami <wizdave97@gmail.com>
2019-02-28 12:25:56 +0100

    Added Player and Enemy class

--- js/app.js
+++ js/app.js
@@ -5,8 +5,21 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = './images/enemy-bug.png';
+    this.x=0;
+    this.speed();
+    this.initLocation();
+    
 };
+let enemyLocations=75;
+Enemy.prototype.initLocation=function(){
+    this.y=enemyLocations;
+    enemyLocations+=75;
+}
+Enemy.prototype.speed=function(){
+    this.speed=Math.floor(Math.random()*300);
+    if(this.speed<30) this.initLocation.speed()
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +27,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x+= this.speed*dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,11 +35,12 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var Player=function(){
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+    this.sprite='./images/char-boy.png'
+    this.initLocation();
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -34,6 +49,27 @@ Enemy.prototype.render = function() {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+}
+Player.prototype.initLocation=function(){
+    this.y=415;
+    this.x=Math.floor(Math.random()*505);
+    if(this.x<=101||this.x>=303){
+        this.initLocation()
+    }
+}
+Player.prototype.render=function(){
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y)
+}
+Player.prototype.update=function(dt){
+
+}
+const player= new Player();
+const allEnemies=[];
+while(allEnemies.length<3){
+    allEnemies.push(new Enemy())
+}
+window.player=player;
+window.allEnemies=allEnemies;
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
--- js/engine.js
+++ js/engine.js
@@ -91,7 +91,7 @@ var Engine = (function(global) {
      */
     function updateEntities(dt) {
         allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
+           enemy.update(dt);
         });
         player.update();
     }
@@ -107,12 +107,12 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                './images/water-block.png',   // Top row is water
+                './images/stone-block.png',   // Row 1 of 3 of stone
+                './images/stone-block.png',   // Row 2 of 3 of stone
+                './images/stone-block.png',   // Row 3 of 3 of stone
+                './images/grass-block.png',   // Row 1 of 2 of grass
+                './images/grass-block.png'    // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -169,11 +169,11 @@ var Engine = (function(global) {
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
+        './images/stone-block.png',
+        './images/water-block.png',
+        './images/grass-block.png',
+        './images/enemy-bug.png',
+        './images/char-boy.png'
     ]);
     Resources.onReady(init);
 
@@ -182,4 +182,5 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
+
 })(this);
